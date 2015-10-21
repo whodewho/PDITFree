75fac26531f09ccb160b5b441074d99079fac5de
chenkai5 <chenkai5@xiaomi.com>
2015-10-21 17:03:51 +0800

    add impl
    N/A
    
    Signed-off-by: chenkai5 <chenkai5@xiaomi.com>

--- pojodaoimpltest.py
+++ pojodaoimpltest.py
@@ -14,12 +14,18 @@ def sqlVarToVar(x):
 def getColsType(c):
     if c in colsType:
         return colsType[c]
-    else:
+    elif "time" in c or "Time" in c:
         return "Timestamp"
+    else:
+        return "NULL"
 
 def sqlVarToDAOParam(c):
     vc = sqlVarToVar(c)
     return "@SQLParam(\""+vc+"\") "+getColsType(c)+" "+vc
+
+def sqlVarToFuncParam(c):
+    vc = sqlVarToVar(c)
+    return getColsType(c)+" "+vc
 
 def makeSqlWhere(c):
     if " " not in c:
@@ -37,8 +43,14 @@ def makeWhereVar(c):
 def makeSqlAssign(c):
     return c+"=:"+sqlVarToVar(c)
 
+def makeFuncSuffix(c):
+    return "And".join([sqlVarToClass(x) for x in c])
+
 state = 0
 daoHeadPrinted = False
+
+findFuncArray=[]
+updateFuncArray=[]
 
 with open('s') as f:
     for line in f:
@@ -54,6 +66,7 @@ with open('s') as f:
             c = line[line.index('`')+1:line.rindex('`')]
             cols.append(c)
 
+            #generate pojo vars
             t = "NULL"
             if "BIGINT" in line:
                 t = "long"
@@ -69,6 +82,7 @@ with open('s') as f:
 
             print "private",t,c+";\n"
         elif 1 == state:
+            #generate dao find
             if not daoHeadPrinted:
                 print "static final String TABLE = \""+table+"\";\n"
                 cols = cols[1:]
@@ -83,30 +97,57 @@ with open('s') as f:
             lineArray = line.split(':')
             whereArray = lineArray[0].split(',')
             print "@SQL(\"SELECT \" + FIELDS + \" FROM \" + TABLE + \" WHERE " + " AND ".join([makeSqlWhere(c) for c in whereArray])+"\")"
-            whereVars = [makeWhereVar(c) for c in whereArray]
+            whereSqlVars = [makeWhereVar(c) for c in whereArray]
+            findFuncArray.append([whereSqlVars, lineArray[1]])
 
             method=""
             if lineArray[1]=="1":
                 method+="POJO"+sqlVarToClass(table)
             else:
                 method+="List<POJO"+sqlVarToClass(table)+">"
-            method+=(" findBy"+"And".join([sqlVarToClass(x) for x in whereVars])+"("+", ".join([sqlVarToDAOParam(x) for x in whereVars])+");\n")
+            method+=(" findBy"+makeFuncSuffix(whereSqlVars)+"("+", ".join([sqlVarToDAOParam(x) for x in whereSqlVars])+");\n")
             print method
         else:
+            #generate dao update
             if "update" in line:
                 continue;
 
             line = line.strip(' #\n')
             lineArray = line.split(':')
             whereArray = lineArray[0].split(',')
-            whereVars = [makeWhereVar(c) for c in whereArray]
-            assignVars = lineArray[1].split(',')
-            print "@SQL(\"UPDATE \" + TABLE + \" SET "+','.join([makeSqlAssign(c) for c in assignVars])+" WHERE" +  " AND ".join([makeSqlWhere(c) for c in whereArray])+"\")"
-            whereVars = [makeWhereVar(c) for c in whereArray]
-            print "void update"+'And'.join([sqlVarToClass(c) for c in assignVars])+"By"+'And'.join([sqlVarToClass(c) for c in whereVars])+"("+", ".join([sqlVarToDAOParam(x) for x in (whereVars+assignVars)])+");\n"
+            whereSqlVars = [makeWhereVar(c) for c in whereArray]
+            assignSqlVars = lineArray[1].split(',')
+            print "@SQL(\"UPDATE \" + TABLE + \" SET "+','.join([makeSqlAssign(c) for c in assignSqlVars])+" WHERE" +  " AND ".join([makeSqlWhere(c) for c in whereArray])+"\")"
+            whereSqlVars = [makeWhereVar(c) for c in whereArray]
+            updateFuncArray.append([whereSqlVars, assignSqlVars])
+
+            print "void update"+makeFuncSuffix(assignSqlVars)+"By"+makeFuncSuffix(whereSqlVars)+"("+", ".join([sqlVarToDAOParam(x) for x in (whereSqlVars+assignSqlVars)])+");\n"
 
 #generate dao save
 values=":p."+", :p.".join(cols)
 print "@SQL(\"INSERT INTO \" + TABLE + \"(\" + FIELDS + \") VALUES ("+ values+")\")"
-print "void save(@SQLParam(\"p\") POJO"+sqlVarToClass(table)+" "+sqlVarToVar(table)+");"
+print "void save(@SQLParam(\"p\") POJO"+sqlVarToClass(table)+" "+sqlVarToVar(table)+");\n"
 
+#print findFuncArray
+#print updateFuncArray
+
+#generate impl
+print "@Autowired"
+dao = sqlVarToVar(table)+"DAO"
+print  sqlVarToClass(table)+"DAO "+dao+";\n"
+
+for func in findFuncArray:
+    method = "public "
+    if  func[1]=="1":
+        method+="POJO"+sqlVarToClass(table)
+    else:
+        method+="List<POJO"+sqlVarToClass(table)+">"
+    method+=(" findBy"+makeFuncSuffix(func[0])+"("+", ".join([sqlVarToFuncParam(x) for x in func[0]])+")")
+    method+=("{"+dao+".findBy"+makeFuncSuffix(func[0])+"("+', '.join([sqlVarToVar(x) for x in func[0]])+");}")
+    print method+"\n"
+
+for func in updateFuncArray:
+    method = "public void update"
+    method += makeFuncSuffix(func[1])+"By"+makeFuncSuffix(func[0])+"("+", ".join([sqlVarToFuncParam(x) for x in func[0]+func[1]])+")"
+    method+=("{"+dao+".update"+ makeFuncSuffix(func[1])+"By"+makeFuncSuffix(func[0])+"("+','.join([sqlVarToVar(x) for x in func[0]+func[1]])+")}")
+    print method+"\n"
